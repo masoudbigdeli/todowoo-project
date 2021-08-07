8b64682290f7f75174011e8013efca798f53217c
Masoud Bigdeli <masoud8bigdeli@gmail.com>
2021-08-07 14:36:13 +0430

    Initial commit

---
+++ todo/views.py
@@ -0,0 +1,31 @@
+from django.db import IntegrityError
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+
+
+def home(request):
+    return render(request, 'todo/home.html')
+
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html',
+                              {'form': UserCreationForm(), 'error': 'User name in not available. Please choose another user name!'})
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+        else:
+            # Tell the user the passwords didn't match
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error':'Passwords did not match'})
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
+
