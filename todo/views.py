3293963d44b9258b6c62221fcb756d95c3303645
Masoud Bigdeli <masoud8bigdeli@gmail.com>
2021-08-08 02:34:16 +0430

    Completed!

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,12 @@
 from django.db import IntegrityError
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
+from django.utils import timezone
+from .forms import TodoForm
+from .models import Todo
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
@@ -34,19 +38,78 @@ def loginuser(request):
     if request.method == 'GET':
         return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
     else:
-        user = authenticate(request, username=request.POST['username'],password=request.POST['password'])
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user == None:
-            return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error':'User name and password did not match!'})
+            return render(request, 'todo/loginuser.html',
+                          {'form': AuthenticationForm(), 'error': 'User name and password did not match!'})
         else:
             login(request, user)
             return redirect('currenttodos')
 
 
+@login_required
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
 
 
+@login_required
+def createtodo(request):
+    if request.method == 'GET':
+        return render(request, 'todo/createtodo.html', {'form': TodoForm()})
+    else:
+        try:
+            form = TodoForm(request.POST)
+            newtodo = form.save(commit=False)
+            newtodo.user = request.user
+            newtodo.save()
+            return redirect('currenttodos')
+        except:
+            return render(request, 'todo/createtodo.html',
+                          {'form': TodoForm(), 'error': 'Bad data inserted! Try again.'})
+
+
+@login_required
 def currenttodos(request):
-    return render(request, 'todo/currenttodos.html')
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'todo/currenttodos.html', {'todos': todos})
+
+
+@login_required
+def completedtodos(request):
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, 'todo/completedtodos.html', {'todos': todos})
+
+
+@login_required
+def viewtodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'GET':
+        form = TodoForm(instance=todo)
+        return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form})
+
+    else:
+        try:
+            form = TodoForm(request.POST, instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form, 'error': 'Bad info'})
+
+
+@login_required
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.datecompleted = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
+
+
+@login_required
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('currenttodos')
--- todowoo/urls.py
+++ todowoo/urls.py
@@ -20,7 +20,7 @@ from todo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', views.home, name='home'),
+
 
     #Auth
 
@@ -30,5 +30,12 @@ urlpatterns = [
 
 
     #Todos
+    path('', views.home, name='home'),
     path('current/', views.currenttodos, name='currenttodos'),
+    path('completed/', views.completedtodos, name='completedtodos'),
+    path('create/', views.createtodo, name='createtodo'),
+    path('todo/<int:todo_pk>/', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete/', views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete/', views.deletetodo, name='deletetodo'),
+
 ]
